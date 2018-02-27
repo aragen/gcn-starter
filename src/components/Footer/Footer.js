566ea12a52413d799b21c01a4f9fc5c548483c1b
setia <setia.negara.91@gmail.com>
2018-02-27 22:48:55 +0700

    change footer and remove watermark

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import { Container } from 'semantic-ui-react'
+import React from 'react';
+import Link from 'gatsby-link';
+import { Grid } from 'semantic-ui-react';
+import './Footer.css';
 
 const Footer = () => (
   <footer>
-  	<Container >
-		<ul>
-		  <li><a href="https://www.contentful.com/" rel="nofollow" target="_blank"><img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" alt="Powered by Contentful" /></a></li>
-		  <li><a href="https://github.com/ryanwiemer/gatsby-starter-gcn" target="_blank">gatsby-starter-gcn</a> by <a href="https://github.com/ryanwiemer" target="_blank">@ryanwiemer</a></li>
-		</ul>
-    </Container>
+		<Grid>
+      <Grid.Column floated='left' width={4}>
+          <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
+            <img src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg" alt="Powered by Contentful" />
+          </a>
+      </Grid.Column>
+		</Grid>
   </footer>
 )
 
